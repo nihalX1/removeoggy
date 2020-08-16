14bb098cb617fc3789c1d5df78e3fadb00981b5d
nihal <nihalpereira1998@gmail.com>
2020-08-17 01:59:48 +0530

    testremove change

--- remover/Main.java
+++ remover/Main.java
@@ -17,6 +17,9 @@ public class Main {
         List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
         return oggys.size() == 0;
     }
+    public static boolean testremove(ArrayList<String> names) {
+        return false;
+    }
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
         removeOggy(names);
